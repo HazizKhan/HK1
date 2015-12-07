17a1dd9adf348c23e6bb791116a6ffd067a788e6
Haziz Feroz Khan <haziz@localhost.localdomain>
2015-12-07 15:54:57 +0500

    HK1 first commit

---
+++ js.js
@@ -0,0 +1,9 @@
+/*** Created by HazizFeroz on 10/19/2015.***/
+var webTitle = "HK1 - v 1.1";
+var developerName = "Haziz Feroz Khan";
+var article1Title = "Samsung Announces the $100 Gear Virtual Reality Headset";
+var article2Title = "Samsung Announces the 950 Pro SSD with Unbelievable Speeds";
+var article3Title = "HEC to Launch Pakistan School on Internet Governance";
+var article4Title = "MICROSOFT BOUGHT POPULAR ANDROID LOCK-SCREEN APP MAKER";
+var article5Title = "NEW NEXUS PHONES, THE CURRENT NEXUS 6";
+var ad = "Advertisement!";
